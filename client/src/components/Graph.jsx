6e8cdebb6827696c8e47b79468ec480133463477
DESKTOP-368HLSI\lenovo <rishidongre519@gmail.com>
2024-09-04 09:58:28 +0530

    label component

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -6,7 +6,8 @@ Chart.register(ArcElement);
 
 const config = {
   data: {
-    datasets: [{
+    datasets: [
+      {
         data: [300, 50, 100],
         backgroundColor: [
           "rgb(255, 99, 132)",
@@ -14,9 +15,10 @@ const config = {
           "rgb(255, 205, 86)",
         ],
         hoverOffset: 4,
-        borderRadius :30,
-        spacing : 10
-      }]
+        borderRadius: 30,
+        spacing: 10,
+      },
+    ],
   },
   options: {
     cutout: 115,
@@ -25,19 +27,20 @@ const config = {
 
 const Graph = () => {
   return (
-    <div className="flex justify-content max-wxs mx-auto">
+    <div className="flex justify-content max-w-xs mx-auto">
       <div className="item">
         <div className="chart relative">
-          <Doughnut {...config}/>
-          <h3 className="mb-4 font-bold title">Total
+          <Doughnut {...config} />
+          <h3 className="mb-4 font-bold title">
+            Total
             <span className="block text-3xl text-emerald-400">${0}</span>
           </h3>
         </div>
+        <div className="flex flex-col py-10 gap-4">
+          {/*Labels*/}
+          <Labels />
+        </div>
       </div>
-      <div className="flex flex-col py-10 gap-">
-        {/*Labels*/}
-        <Labels/>   
-     </div>
     </div>
   );
 };
