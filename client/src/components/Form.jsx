6ba991a81b90f75154b7bf6461960087357867de
DESKTOP-368HLSI\lenovo <rishidongre519@gmail.com>
2024-09-04 23:38:50 +0530

    Component update for frontend view

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,26 +1,37 @@
+import {useForm} from 'react-hook-form';
+import History from './History';
+
 
 const Form = () => {
+    const {register, handleSubmit, resetField} = useForm();
+
+    const onSubmit =(data)=>{
+        console.log(data)
+    }
   return (
+
+
     <div className="form max-w-sm mx-auto w-96">
         <h1 className="font-bold pb-4 text-xl">Transaction</h1>
-        <form id="form">
+        <form id="form" onSubmit={handleSubmit(onSubmit)}>
             <div className="grid gap-4">
                 <div className="input-group">
-                    <input type="text" placeholder="Salary, House Rent, SIP" className="form-input" />
+                    <input type="text" {...register('name')} placeholder="Salary, House Rent, SIP" className="form-input" />
                 </div>
-                <select className="form-input">
+                <select className="form-input" {...register('type')}>
                     <option value="Investment" defaultValue>Investment</option>
                     <option value="Expense">Expense</option>
                     <option value="Saving">Saving</option>
                 </select>
                 <div className="input-group">
-                    <input type="text" placeholder="Amount" className="form-input" />
+                    <input type="text" {...register('amount')} placeholder="Amount" className="form-input" />
                 </div>
                 <div className="submit-btn">
                     <button className="border py-2 text-white bg-indigo-500 w-full">Make Transaction</button>
                 </div>
             </div>
         </form>
+        <History/>
     </div>
   )
 }
